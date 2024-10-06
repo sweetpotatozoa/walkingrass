c10d6dde5bdd6b53090a75f07dd1ea24a19e85c7
sweetpotatozoa <tkddn3200@korea.ac.kr>
2024-10-06 18:00:26 +0900

    fix : 프리즈마 클라이언트 핫리로드 오류 수정

--- lib/prismaClient.ts
+++ lib/prismaClient.ts
@@ -1,19 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-  // PrismaClient를 전역에서 재사용할 수 있도록 선언
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined // 전역 prisma 변수 선언
 }
 
-let prisma: PrismaClient
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'], // 로그를 활성화하여 쿼리 상태 확인 가능
+  })
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma
